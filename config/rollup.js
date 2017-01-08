3a7e01fbdf7b99c25798546977fcc6735ed402b2
Luke Edwards <luke.edwards05@gmail.com>
2017-01-08 11:02:18 -0800

    add preact/aliases to rollup config

--- config/rollup.js
+++ config/rollup.js
@@ -1,3 +1,4 @@
+const alias = require('rollup-plugin-alias');
 const buble = require('rollup-plugin-buble');
 const replace = require('rollup-plugin-replace');
 const resolve = require('rollup-plugin-node-resolve');
@@ -5,6 +6,11 @@ const resolve = require('rollup-plugin-node-resolve');
 module.exports = {
 	rollup: {
 		plugins: [
+			alias({
+				// you may need `preact-compat` instead!
+				'react': 'preact/aliases',
+				'react-dom': 'preact/aliases'
+			}),
 			buble({
 				jsx: 'h',
 				transforms: {
